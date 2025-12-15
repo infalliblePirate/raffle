64c6ebad80218b50d75a70bd6da574de07cd54cd
infalliblePirate <pavlenko.kateryna@lll.kpi.ua>
2025-12-15 15:58:15 +0100

    refactor: cleanup

--- contracts/chainlink/VRFCoordinatorMock.sol
+++
@@ -1,8 +0,0 @@
-// // SPDX-License-Identifier: MIT
-// pragma solidity ^0.8.9;
-
-// import  "@chainlink/contracts/src/v0.8/mocks/vrf/mocks/VRFCoordinatorV2Mock.sol";
-
-// contract VRFCoordinatorMock is VRFCoordinatorV2Mock {
-//     constructor() VRFCoordinatorV2Mock(0, 0) {}
-// }
